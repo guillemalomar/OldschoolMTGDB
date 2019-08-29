d907c9dab5af38a7d796b4047b527fefe0b89297
guillemalomar <alomarguillem@nebutec.cn>
2019-08-29 10:22:19 +0800

    Djongo successfully tested and added

--- oldschoolmtgdb/settings.py
+++ oldschoolmtgdb/settings.py
@@ -81,10 +81,19 @@ WSGI_APPLICATION = 'oldschoolmtgdb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'decks',
     }
 }
 
